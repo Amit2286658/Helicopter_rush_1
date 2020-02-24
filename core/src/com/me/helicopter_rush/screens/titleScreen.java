3c12e7db313fbcf85f659f45cb6f94fcc3d8f1da
Amit kumar <amitkumar13234353@gmail.com>
2020-02-24 08:31:02 +0530

    title screen now uses scene2d.

--- core/src/com/me/helicopter_rush/screens/titleScreen.java
+++ core/src/com/me/helicopter_rush/screens/titleScreen.java
@@ -3,14 +3,19 @@ package com.me.helicopter_rush.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.me.helicopter_rush.constants;
 import com.me.helicopter_rush.helicopter_rush;
 
 public class titleScreen extends screen {
 
-    private Texture /*startGame, about, */background, playbutton;
+    private Texture backgroundtex, playbuttontex, titletex;
     private helicopter_rush rush;
+    Stage uiStage;
 
     public titleScreen(helicopter_rush rush) {
         super(rush.getBatch(), true);
@@ -20,14 +25,28 @@ public class titleScreen extends screen {
     @Override
     public void show() {
         super.show();
-        /*startGame = new Texture("start_game.png");
-        about = new Texture("about.png");*/
-        playbutton = new Texture("playbutton.png");
-        background = new Texture("bg_new.png");
+        playbuttontex = new Texture("playbutton.png");
+        backgroundtex = new Texture("bg_new.png");
+        titletex = new Texture("title.png");
 
-        Gdx.input.setInputProcessor(this);
         //getUiCamera().setToOrtho(false, constants.GAME_WIDTH, constants.GAME_HEIGHT);
         getUiCamera().position.set(getUiViewPort().getWorldWidth()/2, getUiViewPort().getWorldHeight()/2, 0);
+
+        playButton playButton = new playButton(playbuttontex, new Vector2(
+                constants.GAME_WIDTH / 2 - playbuttontex.getWidth() / 2,
+                (constants.GAME_HEIGHT / 2 - playbuttontex.getHeight() / 2)), 104, 58, rush);
+        title title = new title(titletex, new Vector2(
+                constants.GAME_WIDTH/2-600/2, constants.GAME_HEIGHT-160),
+                600, 100);
+        background background = new background(backgroundtex,
+                new Vector2(constants.ORIGIN.x, constants.ORIGIN.y), constants.GAME_WIDTH, constants.GAME_HEIGHT);
+
+        this.uiStage = new Stage(getUiViewPort(), rush.getBatch());
+        uiStage.addActor(background);
+        uiStage.addActor(title);
+        uiStage.addActor(playButton);
+
+        Gdx.input.setInputProcessor(uiStage);
     }
 
     @Override
@@ -40,16 +59,7 @@ public class titleScreen extends screen {
         batch.setProjectionMatrix(getUiCamera().combined);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        batch.draw(background, constants.ORIGIN.x, constants.ORIGIN.y, constants.GAME_WIDTH, constants.GAME_HEIGHT);
-        batch.draw(playbutton, constants.GAME_WIDTH / 2 - playbutton.getWidth() / 2,
-                (constants.GAME_HEIGHT / 2 - playbutton.getHeight() / 2), 104, 58);
-        /*batch.draw(startGame, constants.GAME_WIDTH / 2 - startGame.getWidth() / 2,
-                (constants.GAME_HEIGHT / 2 - startGame.getHeight() / 2) + 60, 412, 78);
-        batch.draw(about, constants.GAME_WIDTH / 2 - about.getWidth() / 2,
-                (constants.GAME_HEIGHT/ 2 - about.getHeight() / 2) - 60, 412, 78);*/
-        batch.end();
+        uiStage.draw();
     }
 
     @Override
@@ -60,19 +70,13 @@ public class titleScreen extends screen {
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
-        getGameViewPort().update(width, height);
         getUiViewPort().update(width, height);
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        /*startGame.dispose();
-        about.dispose();*/
-        playbutton.dispose();
-        background.dispose();
-        //this.batch.dispose();
-        //this.rush.dispose();
+        uiStage.dispose();
     }
 
     @Override
@@ -83,11 +87,55 @@ public class titleScreen extends screen {
                 && screenY >= ((constants.GAME_HEIGHT/2-playbutton.getHeight()/2))
                 && screenY <= (constants.GAME_HEIGHT/2+playbutton.getHeight()/2)){
         }*/
-        rush.setScreen(new gameScreen(
-                rush
-        ));
-
         return true;
+    }
+
+    static class playButton extends actor{
+        playButton(Texture tex, Vector2 pos, int width, int height, final helicopter_rush rush) {
+            super(tex, pos, width, height);
+
+            setTouchable(Touchable.enabled);
+            addListener(new InputListener(){
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    rush.setScreen(
+                            new gameScreen(
+                                    rush
+                            )
+                    );
+                    return true;
+                }
+            });
+        }
+    }
+    static class title extends actor{
+        title(Texture tex, Vector2 pos, int width, int height) {
+            super(tex, pos, width, height);
+        }
+    }
+    static class background extends actor{
+        background(Texture tex, Vector2 pos, int width, int height) {
+            super(tex, pos, width, height);
+        }
+    }
+
+    static abstract class actor extends Actor{
+        Texture tex;
+        Vector2 pos;
+        int width, height;
+
+        actor(Texture tex, Vector2 pos, int width, int height){
+            this.tex = tex;
+            this.pos = pos;
+            this.width = width;
+            this.height = height;
+            setBounds(this.pos.x, this.pos.y, width, height);
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            batch.draw(tex, this.pos.x, this.pos.y, width, height);
+        }
     }
 }
 
